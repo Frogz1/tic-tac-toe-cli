3f803708c85bdc6e9acb068dcda7100d4ccb73a0
Trevor Old <trevor.old@outlook.com>
2018-04-11 15:09:25 -0700

    running into issues creating a listener

--- game.js
+++ game.js
@@ -1,3 +1,9 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var util = require('util');
+
+
+
 const board = () => {
   console.log("  1 2 3")
   let board = "1|_|_|_|\n2|_|_|_|\n3|_|_|_|".split('');
@@ -20,24 +26,71 @@ const board = () => {
 }
 
 
-board()
+
 
 
 class TicTacToeCli {
   constructor() {
     this.board = "1|_|_|_|\n2|_|_|_|\n3|_|_|_|".split('');
-  }
-
-  welcomeMessage() {
-    console.log('Welcome to a wonderful game of CLI Tic-Tac-Toe!!');
-    console.log('Player 1 = X\nPLayer 2 = O\nPlayer 1 goes first, type the coordinate in n,n format to set your piece!')
-    console.log("Type 'start' to begin!")
-    let cmd = process.argv[0];
-    if (cmd === 'start') {
-      console.log('game started!')
+    this.player = 'X',
+    this.complete = false;
+    this.message = '';
+    this.readMessage();
+    this.positionMap = {
+      '1,1': 2,
+      '1,2': 4,
+      '1,3': 6,
+      '2,1': 11,
+      '2,2': 13,
+      '2,3': 15,
+      '3,1': 20,
+      '3,2': 22,
+      '3,3': 24
     }
   }
-}
+  printBoard() {
+    console.log("  1 2 3")
+    console.log(this.board.join(''))
+  }
+  welcomeMessage() {
+    console.log('Welcome to a wonderful game of CLI Tic-Tac-Toe!!');
+    console.log("  1 2 3")
+    console.log(this.board.join(''))
+    console.log('Player 1 = X\nPLayer 2 = O\nPlayer 1 goes first, type the coordinate in n,n format to set your piece!')
+    console.log("Type 'start' to begin!")
 
-game = new TicTacToeCli();
-game.welcomeMessage()
+   
+      if (this.message === 'quit') {
+        this.endGame();
+      } else if (this.message === 'start\n') {
+        console.log('Waiting for player 1...')
+      } else {
+        this.setPiece(this.message)
+      }
+    }
+  
+
+  readMessage() {
+    process.stdin.on('data', function (text) {
+      // console.log('received data:', util.inspect(text));
+      this.message = text.split('\n')[0];
+    })
+  }
+  
+  setPiece(pos) {
+
+    this.board[this.positionMap[pos]] = this.player;
+    this.player === 'X' ? this.player = 'O' : this.player = 'X';
+
+  }
+
+  endGame() {
+    process.exit()
+  }
+      
+  };
+
+var newGame = new TicTacToeCli();
+
+newGame.welcomeMessage()
+
